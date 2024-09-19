5be38f96628aefc8b56a27f8315d5ce96c7284dd
Nicholas Perron <nperron2@oakland.edu>
2024-09-19 12:21:09 -0400

    Uploading Program

--- src/App.java
+++ src/App.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner stringInput = new Scanner(System.in);
+        System.out.println("Please enter the string that you want to reverse:");
+
+        String stringtoReverse = stringInput.nextLine();
+
+        for(int i = stringtoReverse.length(); i > 0; i--){
+            System.out.print(stringtoReverse.charAt(i-1));
+        }
     }
 }
