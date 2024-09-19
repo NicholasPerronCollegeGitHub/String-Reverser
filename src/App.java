0eb958a97a06201fbc4b0293c4e353ba5a97daf5
Nicholas Perron <nperron2@oakland.edu>
2024-09-19 12:30:12 -0400

    Converted to .jar

--- src/App.java
+++ src/App.java
@@ -1,13 +1,14 @@
 import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        Scanner stringInput = new Scanner(System.in);
-        System.out.println("Please enter the string that you want to reverse:");
+        Scanner input = new Scanner(System.in);
+            System.out.println("Please enter the string that you want to reverse:");
 
-        String stringtoReverse = stringInput.nextLine();
+            String stringtoReverse = input.nextLine();
 
-        for(int i = stringtoReverse.length(); i > 0; i--){
-            System.out.print(stringtoReverse.charAt(i-1));
+            for(int i = stringtoReverse.length(); i > 0; i--){
+                System.out.print(stringtoReverse.charAt(i-1));
+        
         }
     }
 }
